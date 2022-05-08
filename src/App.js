4beed127dabf543916be7effc340cf981ebc7933
Allef Rodrigo Schmidt <dev.allefschmidt@gmail.com>
2022-05-08 19:48:09 -0300

    First things first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PriceComponent from '../src/components/pricing/pricing'
+function App() {
+  return (
+    <div className="App">
+      
+      <PriceComponent/>
+    </div>
+  );
+}
+
+export default App;
